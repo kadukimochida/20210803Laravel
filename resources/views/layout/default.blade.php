4461b31dd2eb079bfd8b67c329081ea70b42011d
kadukimochida <dolce.km.3a@gmail.com>
2021-08-03 16:07:32 +0900

    firstcommit

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<style>
+  body {
+    background-color: #000080;
+  }
+  .todolist {
+    background-color: white;
+    width: 70%;
+    margin: 15%;
+    border-radius: 15px;
+  }
+  h1 {
+    padding: 20px;
+  }
+</style>
+<body>
+  <div class="todolist">
+    <h1>Todo List</h1>
+    <div class="content">
+    @yield('content')
+    </div>
+  </div>
+</body>
+</html>
